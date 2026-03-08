987e36a7a256a75bcf80aeb6c32fa9f5fbd75b8f
Zhenia <zhenia.saharow@yandex.ru>
2026-03-08 21:35:56 +0300

    Update Lab1

--- F#/Lab1/task1.fsx
+++ F#/Lab1/task1.fsx
@@ -1,28 +1,40 @@
-let a = 0.0
-let b = 1.0
+let a = 0.
+let b = 1.
 let segments = 10
 let eps = 1e-10
 
+let iter f i a b =
+    let rec iter' a acc =
+        match a > b with
+        | true -> acc
+        | false -> iter' (a + 1) (f a acc)
+
+    iter' a i
+
+let power x =
+    iter (fun _ acc -> acc * x) 1. 1
+
 let builtin x =
-    1.0 / (4.0 - pown x 4)
+    1. / (4. - power x 4)
 
 let rec sumUntil getTerm nextState state sum terms =
     let term = getTerm state
 
-    if abs term < eps then
-        sum, terms
-    else
-        sumUntil getTerm nextState (nextState state) (sum + term) (terms + 1)
+    let continueOrStop = function
+        | true -> sumUntil getTerm nextState (nextState state) (sum + term) (terms + 1)
+        | false -> sum, terms
+
+    continueOrStop (abs term >= eps)
 
 let dumbTerm x n =
-    pown x (4 * n) / pown 4.0 (n + 1)
+    power x (4 * n) / power 4. (n + 1)
 
 let dumbTaylor x =
-    sumUntil (dumbTerm x) (fun n -> n + 1) 0 0.0 0
+    sumUntil (dumbTerm x) (fun n -> n + 1) 0 0. 0
 
 let smartTaylor x =
-    let ratio = pown x 4 / 4.0
-    sumUntil id (fun term -> term * ratio) 0.25 0.0 0
+    let ratio = power x 4 / 4.
+    sumUntil id (fun term -> term * ratio) 0.25 0. 0
 
 let printHeader =
     printfn "f(x) = 1 / (4 - x^4), x in [%.1f; %.1f], eps = %.1e" a b eps
@@ -30,23 +42,25 @@ let printHeader =
     printfn "-----------------------------------------------------------------------------------------------"
 
 let rec printRows i =
-    if i > segments then
-        ()
-    else
-        let x = a + float i * (b - a) / float segments
-        let fx = builtin x
-        let smartValue, smartTerms = smartTaylor x
-        let dumbValue, dumbTerms = dumbTaylor x
+    let proceed = function
+        | true ->
+            let x = a + float i * (b - a) / float segments
+            let fx = builtin x
+            let smartValue, smartTerms = smartTaylor x
+            let dumbValue, dumbTerms = dumbTaylor x
 
-        printfn "%6.3f | %14.10f | %14.10f | %7d | %14.10f | %7d"
-            x
-            fx
-            smartValue
-            smartTerms
-            dumbValue
-            dumbTerms
+            printfn "%6.3f | %14.10f | %14.10f | %7d | %14.10f | %7d"
+                x
+                fx
+                smartValue
+                smartTerms
+                dumbValue
+                dumbTerms
 
-        printRows (i + 1)
+            printRows (i + 1)
+        | false -> ()
+
+    proceed (i <= segments)
 
 let printTable =
     printHeader
--- F#/Lab1/task2.fsx
+++ F#/Lab1/task2.fsx
@@ -1,6 +1,14 @@
 let eps = 1e-10
 let maxIterations = 10000
 
+let iter f i a b =
+    let rec iter' a acc =
+        match a > b with
+        | true -> acc
+        | false -> iter' (a + 1) (f a acc)
+
+    iter' a i
+
 type SolveResult =
     | Converged of float * int
     | Diverged of int
@@ -9,19 +17,18 @@ type SolveResult =
 let dichotomy f a b =
     let fa = f a
 
-    if fa * f b > 0.0 then
+    if f a * f b > 0. then
         DomainError "Dichotomy requires opposite signs on [a, b]"
     else
-
         let rec loop left right fLeft iter =
-            let mid = (left + right) / 2.0
+            let mid = (left + right) / 2.
             let fMid = f mid
 
-            if abs fMid < eps || abs (right - left) / 2.0 < eps then
+            if abs fMid < eps || abs (right - left) / 2. < eps then
                 Converged (mid, iter + 1)
             elif iter >= maxIterations then
                 Diverged (iter + 1)
-            elif fLeft * fMid <= 0.0 then
+            elif fLeft * fMid <= 0. then
                 loop left mid fLeft (iter + 1)
             else
                 loop mid right fMid (iter + 1)
@@ -41,19 +48,19 @@ let iterations phi x0 =
 
     loop x0 0
 
-let newton f derivative x0 =
+let newton f f' x0 =
     let phi x =
-        let d = derivative x
+        let d = f' x
 
-        if abs d < 1e-14 then
-            nan
-        else
-            x - f x / d
+        let safeStep = function
+            | true -> nan
+            | false -> x - f x / d
+
+        safeStep (abs d < 1e-14)
 
     iterations phi x0
 
-let formatCell result =
-    match result with
+let formatCell = function
     | Converged (root, iterations) -> sprintf "%.8f (%d)" root iterations
     | Diverged iterations -> sprintf "DIVERGED (%d)" iterations
     | DomainError reason -> sprintf "ERROR: %s" reason
@@ -71,28 +78,28 @@ let printRow number dResult iResult nResult =
         nCell
 
 let f25 x =
-    sqrt (1.0 - 0.4 * x * x) - asin x
+    sqrt (1. - 0.4 * x * x) - asin x
 
 let f26 x =
-    exp x - exp (-x) - 2.0
+    exp x - exp (-x) - 2.
 
 let f27 x =
-    sin (log x) - cos (log x) + 2.0 * log x
+    sin (log x) - cos (log x) + 2. * log x
 
 let f25' x =
-    -0.4 * x / sqrt (1.0 - 0.4 * x * x) - 1.0 / sqrt (1.0 - x * x)
+    -0.4 * x / sqrt (1. - 0.4 * x * x) - 1. / sqrt (1. - x * x)
 
 let f26' x =
     exp x + exp (-x)
 
 let f27' x =
-    (cos (log x) + sin (log x) + 2.0) / x
+    (cos (log x) + sin (log x) + 2.) / x
 
 let phi25 x =
-    sin (sqrt (1.0 - 0.4 * x * x))
+    sin (sqrt (1. - 0.4 * x * x))
 
 let phi26 x =
-    log (2.0 + exp (-x))
+    log (2. + exp (-x))
 
 let phi27 x =
     x - 0.4 * f27 x
@@ -104,13 +111,13 @@ let solveOne f f' phi a b x0 =
     dResult, iResult, nResult
 
 let solveVariant25 =
-    solveOne f25 f25' phi25 0.0 1.0 0.5
+    solveOne f25 f25' phi25 0. 1. 0.5
 
 let solveVariant26 =
-    solveOne f26 f26' phi26 0.0 1.0 0.5
+    solveOne f26 f26' phi26 0. 1. 0.5
 
 let solveVariant27 =
-    solveOne f27 f27' phi27 1.0 3.0 1.5
+    solveOne f27 f27' phi27 1. 3. 1.5
 
 printfn "%-4s | %28s | %28s | %28s" "#" "Dichotomy" "Iterations" "Newton"
 printfn "------------------------------------------------------------------------------------------------------"
@@ -119,6 +126,11 @@ let d25, i25, n25 = solveVariant25
 let d26, i26, n26 = solveVariant26
 let d27, i27, n27 = solveVariant27
 
-printRow 25 d25 i25 n25
-printRow 26 d26 i26 n26
-printRow 27 d27 i27 n27
+let allRows =
+    [| 25, d25, i25, n25
+       26, d26, i26, n26
+       27, d27, i27, n27 |]
+
+iter (fun idx () ->
+    let number, dResult, iResult, nResult = allRows.[idx]
+    printRow number dResult iResult nResult) () 0 (allRows.Length - 1)
